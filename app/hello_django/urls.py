98bddd350d99acae73190b24ad246772e084951d
Hemant Badhe <hemantbbadhe@gmail.com>
2020-01-31 00:49:06 +0530

    initial commit

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from ..upload.views import image_upload
 
 urlpatterns = [
-    path("", image_upload, name="upload"),
+    path("", include('upload.urls')),
     path("admin/", admin.site.urls),
 ]
 
